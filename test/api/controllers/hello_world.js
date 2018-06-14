86aeb5d85a15a301f1de37ff39bebf69c31a5619
VINCENT GONZALES <vgonzal@us.ibm.com>
2018-06-14 18:16:22 +0000

    added there

--- test/api/controllers/hello_world.js
+++ test/api/controllers/hello_world.js
@@ -20,7 +20,7 @@ describe('controllers', function() {
           .end(function(err, res) {
             should.not.exist(err);
 
-            res.body.should.eql('Hello, stranger!');
+            res.body.should.eql('Hello there, stranger from test!');
 
             done();
           });
@@ -39,7 +39,7 @@ describe('controllers', function() {
           .end(function(err, res) {
             should.not.exist(err);
 
-            res.body.should.eql('Hello, Scott!');
+            res.body.should.eql('Hello there, Scott from test!');
 
             done();
           });
